b4b2f9845761b83d33e7b25150fcd7ce65b11e36
Zaed Abdullah <z4ed.thalib123@gmail.com>
2023-01-11 21:20:56 +0700

    add user seeder & upgrade to laravel 9

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => Str::random(10),
+            'email' => Str::random(10),
+            'password' => Str::random(10),
+        ]);
+    }
+}
